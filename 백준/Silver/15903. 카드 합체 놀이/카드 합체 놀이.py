f337566e8772e7f8af560da876823b8edfee2a5c
kuk329 <74480236+kuk329@users.noreply.github.com>
2024-01-09 22:19:33 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 60 ms, Memory: 33320 KB -BaekjoonHub

--- 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.py
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.py
@@ -1,16 +1,14 @@
-n,m = map(int,input().split())
-card = list(map(int,input().split())) 
+from heapq import heappush, heappop, heapify
 
-def play():
-    # 카드에서 가장 작은 값 두개를 찾아서 합친뒤 그 값을 다시 넣어준다.
-    card.sort() # 오름차순 정렬
-    add=card[0]+card[1]
-    card[0]=add
-    card[1]=add
+n, m = map(int, input().split())
+cards = list(map(int, input().split()))
 
+heapify(cards)
 for _ in range(m):
-    play()
+    first = heappop(cards)
+    second = heappop(cards)
+    add = first + second
+    heappush(cards, add)
+    heappush(cards,add)
 
-print(sum(card))
-    
-
+print(sum(cards))
