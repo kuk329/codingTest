8302f45eafda3fce2bf0625dae8578eace4e8c87
kuk329 <74480236+kuk329@users.noreply.github.com>
2024-02-22 22:26:06 +0900

    [Gold IV] Title: 주사위 굴리기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.py
@@ -0,0 +1,54 @@
+import sys
+input = sys.stdin.readline
+
+N,M,x,y,K = map(int,input().split())
+
+a = []
+dice = [0]*7
+for _ in range(N):
+    tmp = list(map(int,input().split()))
+    a.append(tmp)
+
+order = list(map(int,input().split()))
+
+dx = [0,0,-1,1] # 동 서 남 북
+dy = [1,-1,0,0]
+
+
+for k in order:
+    k -= 1
+    nx,ny = x+dx[k],y+dy[k]
+    if nx < 0 or nx >= N or ny < 0 or ny >= M:
+        continue
+    if k == 0:
+        temp = dice[1]
+        dice[1] = dice[4]
+        dice[4] = dice[6]
+        dice[6] = dice[3]
+        dice[3] = temp
+    elif k == 1:
+        temp = dice[1]
+        dice[1] = dice[3]
+        dice[3] = dice[6]
+        dice[6] = dice[4]
+        dice[4] = temp
+    elif k == 2:
+        temp = dice[1]
+        dice[1] = dice[5]
+        dice[5] = dice[6]
+        dice[6] = dice[2]
+        dice[2] = temp
+    else:
+        temp = dice[1]
+        dice[1] = dice[2]
+        dice[2] = dice[6]
+        dice[6] = dice[5]
+        dice[5] = temp
+    x,y = nx,ny
+    if a[x][y] == 0:
+        a[x][y] = dice[6]
+    else:
+        dice[6] = a[x][y]
+        a[x][y] = 0
+        
+    print(dice[1])
