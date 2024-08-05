56df2f432f161261b0363ea171ddb113c0420e19
kuk329 <74480236+kuk329@users.noreply.github.com>
2024-08-05 22:21:20 +0900

    [Silver II] Title: 예산, Time: 40 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.py
+++ 백준/Silver/2512. 예산/예산.py
@@ -1,32 +1,38 @@
-# 가능한 최대의 예산으로 분배
-
 N = int(input()) # 지방의 수
-arr = list(map(int,input().split()))
-arr.sort() # 110 120 140 150
-limit = int(input()) # 총 예산의 수
+budget = list(map(int,input().split()));
+M = int(input())
+budget.sort()
+
+left = 0
+right = budget[-1]
+
+def total(mid):
+    result = 0
+    for i in range(N):
+        if budget[i]<mid:
+            result+=budget[i]
+        else:
+            result += mid
+
+    return result
+
+max_money = 0
+while left<=right:
+    mid = (left+right)//2
+    add = total(mid)
+    if add > M:
+        right = mid -1
+    # elif add==M:
+    #     print(mid)
+    #     break
+    else:
+        if max_money<mid:
+            max_money=mid
+        left = mid +1
 
 
-def go(s,e):
-    max_money = 0
 
-    while s<=e:
-        add = 0
-        mid = (s+e)//2 # 각 지방마다 분배할 예산
-        for i in range(N):
-            if arr[i]<mid:
-                add+=arr[i]
-            else:
-                add+=mid
-        if add > limit: # 지방에 나누어준 예산합이 총 예산보다 크면
-            e = mid - 1 # 예산수를 줄임
-        else: # 지방에 나누어준 예산합이 총 예산보다 작으면 (가능한 경우들)
-            if max_money < mid:
-                max_money = mid
-            s = mid + 1
-
-    return max_money
+print(max_money)
 
 
 
-
-print(go(1,arr[-1])) # 각 지방마다 줄수있는 예산의 범위 구하기
